652d0f8975566c52e416d27eb8e7ac08507f0751
baris <baris.ock.etudes@gmail.com>
2025-09-08 09:33:19 +0200

    suppression map.tmx

--- maps/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="map" tilewidth="16" tileheight="16" tilecount="6480" columns="120">
- <image source="tileset/map.png" trans="000000" width="1920" height="864"/>
-</tileset>
